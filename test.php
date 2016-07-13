eb17f3c541a41bf7cdb5c2da89ab7b4780eb9c0e
zuhriutama <zuhriutamaor@gmail.com>
2016-07-13 11:42:58 +0700

    testing json

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'http://api.globalquran.com/ayah/1:1/id.indonesian?key=d1bdfe6421908ff4cfb71fd1e7630e0b');
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result->verse;
+?>
