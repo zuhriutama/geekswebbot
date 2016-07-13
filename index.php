e50c2c8cb88251226d3449e7433e94d47fc833f7
zuhriutama <zuhriutamaor@gmail.com>
2016-07-13 10:16:29 +0700

    changed all using bobbot
    https://github.com/D4RKONION/bobbot/

--- index.php
+++ index.php
@@ -1,78 +1,28 @@
 <?php
 
-/*
-* This file is part of GeeksWeb Bot (GWB).
-*
-* GeeksWeb Bot (GWB) is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 3
-* as published by the Free Software Foundation.
-* 
-* GeeksWeb Bot (GWB) is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.  <http://www.gnu.org/licenses/>
-*
-* Author(s):
-*
-* © 2015 Kasra Madadipouya <kasra@madadipouya.com>
-*
-*/
-require 'vendor/autoload.php';
+//set up the Bot API token
+$botToken = "188202276:AAEZOAis8JyBQ8cQEtssKtzIH-9CEdCJPHg"; // @mr_marbot
+$website = "https://api.telegram.org/bot".$botToken;
 
-$client = new Zelenin\Telegram\Bot\Api('188202276:AAEZOAis8JyBQ8cQEtssKtzIH-9CEdCJPHg'); // Set your access token
-$url = 'http://zuhriutama.com/feed'; // URL RSS feed
-$update = json_decode(file_get_contents('php://input'));
+//Grab the info from the webhook, parse it and put it into $message
+$content = file_get_contents("php://input");
+$update = json_decode($content, TRUE);
+$message = $update["message"];
 
-//your app
-try {
+//Make some helpful variables
+$chatId = $message["chat"]["id"];
+$text = $message["text"];
 
-    if($update->message->text == '/email')
-    {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-        	'chat_id' => $update->message->chat->id,
-        	'text' => "You can send email to : hallo@zuhriutama.com"
-     	]);
-    }
-    else if($update->message->text == '/help')
-    {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-    		'chat_id' => $update->message->chat->id,
-    		'text' => "List of commands :\n /email -> Get email address of the owner \n /latest -> Get latest posts of the blog 
-    		/help -> Shows list of available commands"
-    		]);
+//Our BobWisdom Array
+$bobWisdom = ["That's a crooked tree. We'll send him to Washington.", "I like to beat the brush.", "In painting, you have unlimited power. You have the ability to move mountains. You can bend rivers. But when I get home, the only thing I have power over is the garbage.", "You need the dark in order to show the light.", "Look around. Look at what we have. Beauty is everywhere—you only have to look to see it.", "Just go out and talk to a tree. Make friends with it.", "There's nothing wrong with having a tree as a friend.", "Trees cover up a multitude of sins.", "They say everything looks better with odd numbers of things. But sometimes I put even numbers—just to upset the critics.", "How do you make a round circle with a square knife? That’s your challenge for the day.", "I remember when my Dad told me as a kid, ‘If you want to catch a rabbit, stand behind a tree and make a noise like a carrot. Then when the rabbit comes by you grab him.’", "We tell people sometimes: we're like drug dealers, come into town and get everybody absolutely addicted to painting. It doesn't take much to get you addicted", "The secret to doing anything is believing that you can do it. Anything that you believe you can do strong enough, you can do. Anything. As long as you believe", "Water's like me. It's laaazy ... Boy, it always looks for the easiest way to do things", "I really believe that if you practice enough you could paint the 'Mona Lisa' with a two-inch brush.", "If I paint something, I don't want to have to explain what it is", "We don't make mistakes. We just have happy accidents.", "The stream can go whatever way you want, it's your little stream", "Happy Little Cloud", "Happy Little Trees", "Sneaky little cloud! Running around here at night", "Brown snow is worse than yellow snow", "and maybe there's a little cloud over here, just a little floater.", "Sometimes you have to scare em out, scare those little rascals out", "The little trees are just there, hiding in your brush and you have to find them", "I don't want you to be unhappy", "Everything in nature is pretty... use it!", "All you need is a dream in your heart", "That'll be our little secret.", "Gotta give him a friend. Like I always say 'everyone needs a friend'.", "We don't know where it goes. We don't really care.", "Any time ya learn, ya gain.", "Be sure to use odorless paint-thinner. If it's not odorless, you'll find yourself working alone very, very quick.", "Clouds are very, very free.", "Tender as a mothers love... And with my mother, that was certainly true.", "Just scrape in a few indications of sticks and twigs and other little things in there. People will think you spend hours doing this.", "Maybe in our world there lives a happy little tree over there.", "Oh, that would make a nice place to fish. I like fishing, but I'm not a very good fisherman. I always throw the fish back into the water, just put a band-aid on his mouth, tap 'im on the patootie and let him on his way. And maybe some day, if I'm lucky, I'll get to catch him again.", "Shwooop. Hehe. You have to make those little noises, or it just doesn't work.", "People look at me like I'm a little strange, when I go around talking to squirrels and rabbits and stuff. That's ok. Thaaaat's just ok.", "Try to imagine that you are a tree. How do you want to look out here?", "We want happy paintings. Happy paintings. If you want sad things, watch the news.", "We're gonna make some big decisions in our little world.", "From all of us here I'd like to wish you happy painting...and God bless you my friend.", "And just go straight in like your going to stab it. And barely touch it...barely touch it.", "This old barn has seen it's better days, it's like me... it's had a rough life"];
 
-    }
-    else if($update->message->text == '/latest')
-    {
-    		Feed::$cacheDir 	= __DIR__ . '/cache';
-			Feed::$cacheExpire 	= '5 hours';
-			$rss 		= Feed::loadRss($url);
-			$items 		= $rss->item;
-			$lastitem 	= $items[0];
-			$lastlink 	= $lastitem->link;
-			$lasttitle 	= $lastitem->title;
-			$message = $lasttitle . " \n ". $lastlink;
-			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-			$response = $client->sendMessage([
-					'chat_id' => $update->message->chat->id,
-					'text' => $message
-				]);
+//pick a random element
+$todaysWisdom = array_rand($bobWisdom, 1);
 
-    }
-    else
-    {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-    		'chat_id' => $update->message->chat->id,
-    		'text' => "Invalid command, please use /help to get list of available commands"
-    		]);
-    }
-
-} catch (\Zelenin\Telegram\Bot\NotOkException $e) {
-
-    //echo error message ot log it
-    //echo $e->getMessage();
-
+//send the random Bob wisdom back
+if($text === "/bro") {
+	file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=".$bobWisdom[$todaysWisdom]);
 }
+
+
+?>
