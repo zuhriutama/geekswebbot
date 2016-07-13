4d1abc39684354ddbd82e9f0c96a1c28e3f6d013
zuhriutama <zuhriutamaor@gmail.com>
2016-07-13 11:50:08 +0700

    set to array

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, 'http://api.globalquran.com/ayah/1:1/id.indonesian?key=d1bdfe6421908ff4cfb71fd1e7630e0b');
 $result = curl_exec($ch);
 curl_close($ch);
-$data = json_decode($result);
+$data = json_decode($result,true);
 var_dump($data);
-echo "<br/>ayat quran : ".$data->quran->verse;
+echo "<br/>ayat quran : ";
 ?>
